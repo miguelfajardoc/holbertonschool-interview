5044c19e5c3d2d7a7c0700837049002bc218afbc
miguelfajardoc <775@holbertonschool.com>
2020-07-22 03:56:19 +0000

    [fix] condition to do not add keys that doesnt work

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -9,13 +9,15 @@ def canUnlockAll(boxes):
     usedKeys = set()
 
     length = len(boxes)
-
+    
     while (unusedKeys):
+        #print("unusedKeys:")
+        #print(unusedKeys)
         useKey = unusedKeys.pop(0)
         newKeys = boxes[useKey]
         usedKeys.add(useKey)
         for key in newKeys:
-            if key not in usedKeys:
+            if key not in usedKeys and key < len(boxes):
                 unusedKeys.append(key)
         
     if sum(usedKeys) == ((length - 1) * length) / 2:
