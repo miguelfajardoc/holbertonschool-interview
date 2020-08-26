5daa894ec4720e63b26baa298c52a6d9889f990a
miguelfajardoc <775@holbertonschool.com>
2020-08-26 01:37:36 +0000

    [add] a auxiliar matrix to non operate over the actualiced iteration data

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -4,6 +4,27 @@
 #include "sandpiles.h"
 
 /**
+ * print_grid - function that print a grid
+ * @grid: the first sandpile
+ * Return: none
+ */
+static void print_grid(int grid[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+/**
  * sandpiles_sum - function that computes the sum of two sandpiles 3*3
  * @grid1: the first sandpiles
  * @grid2: the second sandpiles
@@ -13,9 +34,10 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int i, j;
 	int flag = 0;
-
+	int iter_matrix[3][3];
 
 	simple_sand_sum(grid1, grid2);
+	copy_matrix(grid1, iter_matrix);
 	do {
 		flag = 0;
 		for (i = 0; i < 3; i++)
@@ -30,13 +52,35 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 						print_grid(grid1);
 						flag++;
 					}
-					make_topple(grid1, i, j);
+					make_topple(iter_matrix, i, j);
 					flag = 1;
 				}
 			}
 		}
+		copy_matrix(iter_matrix, grid1);
 	} while (flag);
 }
+
+/**
+ * copy_matrix - function that copy the data of the first matrix into the
+ * second that have 3x3 dimension
+ * @matrix1: the first matrix
+ * @matrix2: the second matrix
+ * Return: none
+ */
+void copy_matrix(int matrix1[3][3], int matrix2[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			matrix2[i][j] = matrix1[i][j];
+		}
+	}
+}
+
 
 /**
  * simple_sand_sum - function that computes the sum of two sandpiles 3*3
@@ -67,8 +111,8 @@ void simple_sand_sum(int grid1[3][3], int grid2[3][3])
  */
 void make_topple(int grid[3][3], int i, int j)
 {
-	printf("mod grid[%d][%d]: %d\n", i, j, grid[i][j]);
-	print_grid(grid);
+	/* printf("mod grid[%d][%d]: %d\n", i, j, grid[i][j]); */
+	/* print_grid(grid); */
 	grid[i][j] -= 4;
 
 	if (i + 1 < 3)
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,9 +1,9 @@
 #ifndef SANDPILES
 #define SADPILES
 
-void print_grid_sum(int grid1[3][3], int grid2[3][3]);
-void print_grid(int grid[3][3]);
-
+/*static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
+  static void print_grid(int grid[3][3]);*/
+void copy_matrix(int from[3][3], int to[3][3]);
 void make_topple(int grid[3][3], int i, int j);
 void simple_sand_sum(int grid1[3][3], int grid2[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
