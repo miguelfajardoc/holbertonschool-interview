20cfeca718cd7740b4aa94f178a7813faf002c98
miguelfajardoc <775@holbertonschool.com>
2020-08-26 01:43:31 +0000

    [add] correct betty in .h

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,8 +1,6 @@
 #ifndef SANDPILES
-#define SADPILES
+#define SANDPILES
 
-/*static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
-  static void print_grid(int grid[3][3]);*/
 void copy_matrix(int from[3][3], int to[3][3]);
 void make_topple(int grid[3][3], int i, int j);
 void simple_sand_sum(int grid1[3][3], int grid2[3][3]);
