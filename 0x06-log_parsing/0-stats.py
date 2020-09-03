d0866de4d7cc9f872eee15267d3be37809b47bd1
miguelfajardoc <775@holbertonschool.com>
2020-09-02 20:21:55 -0500

    [fix] style

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,13 +3,13 @@
 import sys
 import signal
 
+
 def main():
     file_size = 0
     status_code_dict = {}
     possible_status_code = [200, 301, 400, 401, 403, 404, 405, 500]
     counter = 0
     try:
-        
         for line in sys.stdin:
             if counter != 10:
                 data = line.split()
@@ -22,20 +22,23 @@ def main():
     except KeyboardInterrupt:
         raise print_logs(file_size, status_code_dict, possible_status_code)
     # raise KeyboardInterrupt
-            
+
+
 def add_status_code_to_dict(status_code, status_code_dict):
-    
+
     if status_code in status_code_dict:
         status_code_dict[status_code] += 1
     else:
         status_code_dict[status_code] = 1
 
+
 def print_logs(file_size, status_codes, status_posibilities):
 
-    print("File size: {}".format(file_size));
+    print("File size: {}".format(file_size))
     for status in status_posibilities:
         if status in status_codes:
             print("{}: {}".format(status, status_codes[status]))
+
 
 if __name__ == '__main__':
     main()
