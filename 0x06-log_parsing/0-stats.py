21da63c74aca1a1afb9dccd4530e16729c27a954
miguelfajardoc <775@holbertonschool.com>
2020-09-02 21:29:56 -0500

    [add] change to string

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,34 +1,46 @@
 #!/usr/bin/python3
-'''
-Script for log parsing
-'''
+""" module that do stats """
+
 import sys
 
 
-if __name__ == "__main__":
-    a = []
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    dictio = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
-              "405": 0, "500": 0}
-    i = 0
+def main():
     file_size = 0
+    status_code_dict = {}
+    possible_status_code = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    counter = 0
     try:
-        for lines in sys.stdin:
-            i += 1
-            a = lines.split(" ")
-            if len(a) > 2:
-                file_size += int(a[-1])
-                if a[-2] in codes:
-                    dictio[a[-2]] += 1
-            if i % 10 == 0:
-                print("File size: {}".format(file_size))
-                for code in codes:
-                    if dictio[code] > 0:
-                        print("{}: {}".format(code, dictio[code]))
+        for line in sys.stdin:
+            if counter != 10:
+                data = line.split()
+                if len(data) > 2:
+                    file_size += (int)(data[-1])
+                    add_status_code_to_dict(data[-2], status_code_dict)
+                    counter += 1
+            else:
+                print_logs(file_size, status_code_dict, possible_status_code)
+                counter = 0
     except Exception:
         pass
     finally:
-        print("File size: {}".format(file_size))
-        for code in codes:
-            if dictio[code] > 0:
-                print("{}: {}".format(code, dictio[code]))
+        print_logs(file_size, status_code_dict, possible_status_code)
+
+
+def add_status_code_to_dict(status_code, status_code_dict):
+
+    if status_code in status_code_dict:
+        status_code_dict[status_code] += 1
+    else:
+        status_code_dict[status_code] = 1
+
+
+def print_logs(file_size, status_codes, status_posibilities):
+
+    print("File size: {}".format(file_size))
+    for status in status_posibilities:
+        if status in status_codes:
+            print("{}: {}".format(status, status_codes[status]))
+
+
+if __name__ == '__main__':
+    main()
